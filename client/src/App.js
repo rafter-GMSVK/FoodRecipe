f1d535bd1ad0570de188a27f3e565a1d2bbebf37
rafter-GMSVK <shanbhagji@gmail.com>
2022-08-16 02:36:27 +0530

    Adding the input component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,36 @@ import Spline from '@splinetool/react-spline';
 
 import './App.css';
 
+
+
 function App() {
+
+    const working = ()=>{
+      console.log("It is workoing?");
+    }
+
+
+
   return (
     <div className="App">
+
+        <h1 >Recipes</h1>
+
         <Spline className ="spline"scene="https://prod.spline.design/DzC5cf2mEGqIW2r1/scene.splinecode" />
 
+
+      
+
         <div className="content">
-          <input type="text"></input>
+
+          <div className="input">
+
+          <input type="text" placeholder='Enter the name of the recipe...'></input>
+          <button onClick={working}>Find my Recipe</button>
+
+
+          </div>
+          
 
         </div>
   
