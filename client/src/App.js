58e601e2193f7d755ff087afda700b7061653ef5
rafter-GMSVK <shanbhagji@gmail.com>
2022-08-15 19:04:15 +0530

    The first few assets

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import Spline from '@splinetool/react-spline';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Spline className ="spline"scene="https://prod.spline.design/DzC5cf2mEGqIW2r1/scene.splinecode" />
+
+        <div className="content">
+          <input type="text"></input>
+
+        </div>
+  
+
+      
+    </div>
+  );
+}
+
+export default App;
